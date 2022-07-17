05149c3c780e66dee04b069a4fdc98a7f7f712a1
jonghyun <jonghyeon2046@gmail.com>
2022-07-17 18:13:21 +0900

    feat: tab component 추가

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
 	id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 const DetailPage: React.FC = () => {
 	const { id } = useParams<Params>();
+	const [selectedTab, setSelectedTab] = useState<Tab>("about");
 
-	return <div>DetailPage id: {id}</div>;
+	const handleClick = (tab: Tab) => {
+		setSelectedTab(tab);
+	};
+
+	return (
+		<div>
+			<Tabs tab={selectedTab} onClick={handleClick} />
+		</div>
+	);
 };
 
 export default DetailPage;
